aadae57661fbcd8533d0b4f6e64337cb417ef3b2
dlobera <delobera@gmail.com>
2022-06-13 22:53:56 -0500

    first forgotten commit

---
+++ server.js
@@ -0,0 +1,31 @@
+// import modules
+
+import express from 'express'
+
+// Create Express app
+
+const app = express()
+
+// Configure the app (app.set)
+
+
+
+// Mount Middleware (app.use)
+
+
+
+// Mount routes
+app.get('/', function(req, res) {
+  res.send('<h1>Students</h1>')
+})
+
+app.get('/home', function(req, res) {
+  res.send('<h1>Home Page</h1>')
+})
+
+
+// Tell the app to listen on port 3000
+
+app.listen(3000, function() {
+  console.log('Listening on port 3000')
+})
